08d41b1b6d54ed903f895503b6c13b4f8f76849d
Joschka Schwarz <40362019+jwarz@users.noreply.github.com>
2020-06-19 13:24:18 +0200

    update packages

--- 02/install_pkgs.R
+++ 02/install_pkgs.R
@@ -5,21 +5,15 @@
 pkgs_cran <- c(
     # File System
     "fs",         # working with the file system
-    
+
     # Import
     "readxl",     # reading excel files
     "writexl",    # saving data as excel files
-    "odbc",       # connecting to databases
-    "RSQLite",    # connecting to SQLite databases
-    
+
     # Tidy, Transform, & Visualize
     "tidyverse",  # dplyr, ggplot2, tibble, tidyr, readr, purrr, stringr, forcats
     "lubridate",  # working with dates and times
-    
-    # Model
-    "tidymodels", # installs broom, infer, recipes, rsample, & yardstick
-    "umap",       # used for visualizing clusters
-    
+
     # Other
     "devtools"    # used to install non-CRAN packages
 )
